bc14f34c062e69a791c683b9e4ec8659da734aeb
allwin_m <allwin.m03@gmail.com>
2025-02-27 16:21:37 +0530

    Add files via upload

--- Type.js
+++ Type.js
@@ -1,26 +1,12 @@
 'use strict';
 
-var isObject = require('../helpers/isObject');
+var ES5Type = require('../5/Type');
 
-// https://262.ecma-international.org/5.1/#sec-8
+// https://262.ecma-international.org/6.0/#sec-ecmascript-data-types-and-values
 
 module.exports = function Type(x) {
-	if (x === null) {
-		return 'Null';
+	if (typeof x === 'symbol') {
+		return 'Symbol';
 	}
-	if (typeof x === 'undefined') {
-		return 'Undefined';
-	}
-	if (isObject(x)) {
-		return 'Object';
-	}
-	if (typeof x === 'number') {
-		return 'Number';
-	}
-	if (typeof x === 'boolean') {
-		return 'Boolean';
-	}
-	if (typeof x === 'string') {
-		return 'String';
-	}
+	return ES5Type(x);
 };
